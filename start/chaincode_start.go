4ba5ea7fc172b6d184c5af81f791abf105900ea2
adilharis2001 <adilharis2001@gmail.com>
2017-06-06 16:28:35 +0530

    Fifth step to POC

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -124,6 +124,7 @@ func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function strin
 func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
 	var key, jsonResp string
 	var err error
+	var fetchedmandate mandate
 	if len(args) != 1 {
 		return nil, errors.New("Incorrect number of arguments. Expecting name of the key to query")
 	}
@@ -133,7 +134,17 @@ func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) 
 		jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
 		return nil, errors.New(jsonResp)
 	}
-	return valAsbytes, nil
+	err = json.Unmarshal(valAsbytes, &fetchedmandate)
+	if err != nil {
+		fmt.Println("Error unmarshalling " + key + "\n err:" + err.Error())
+		return fetchedmandate, errors.New("Error unmarshalling " + key)
+	}
+	fetchedmandateBytes, err1 := json.Marshal(&fetchedmandate)
+			if err1 != nil {
+				fmt.Println("Error marshalling the company")
+				return nil, err1
+			}
+	return fetchedmandateBytes, nil
 }
 
 func addNewMandateEntry(name string,bank string,DoB string) {
